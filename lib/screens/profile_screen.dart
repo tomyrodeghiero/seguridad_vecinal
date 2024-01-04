f404658d0abae5d6b8dca90e6cbabfdc210b7ea7
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2024-01-03 22:06:38 -0300

    Add Drawer

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:seguridad_vecinal/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -19,7 +22,7 @@ class ProfileScreen extends StatelessWidget {
           child: IconButton(
             icon: Icon(Icons.arrow_back, color: Colors.white),
             onPressed: () {
-              Navigator.of(context).pop();
+              _scaffoldKey.currentState?.openDrawer();
             },
           ),
         ),
@@ -29,47 +32,131 @@ class ProfileScreen extends StatelessWidget {
         titleSpacing: 0,
         centerTitle: false,
       ),
+      drawer: Drawer(
+        backgroundColor: AppColors.waterGreen400,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: <Widget>[
+                    Container(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 40,
+                              child: Text('L',
+                                  style: TextStyle(
+                                      fontSize: 40.0, color: Colors.white)),
+                            ),
+                            SizedBox(
+                              height: 12.0,
+                            ),
+                            Text(
+                              'Luciana',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'González',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text('Editar perfil',
+                                      style: TextStyle(color: Colors.white)),
+                                  SizedBox(width: 8),
+                                  Icon(Icons.edit, color: Colors.white),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      padding: EdgeInsets.all(16),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.lock_outline, color: Colors.white),
+                      title: Text('Privacidad',
+                          style: TextStyle(color: Colors.white)),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.settings, color: Colors.white),
+                      title: Text('Configuración',
+                          style: TextStyle(color: Colors.white)),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.help_outline, color: Colors.white),
+                      title: Text('Centro de ayuda',
+                          style: TextStyle(color: Colors.white)),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Container(
-        color: Colors.white, // Set the background color to white
+        color: Colors.white,
         child: Column(
           children: <Widget>[
-            SizedBox(height: 80), // Provide spacing from the top
+            SizedBox(height: 80),
             Stack(
-              alignment:
-                  Alignment.center, // Align the stack contents to the center
+              alignment: Alignment.center,
               children: <Widget>[
                 Container(
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(
-                      color: Colors.black, // Border color
-                      width: 1.0, // Border width
+                      color: Colors.black,
+                      width: 1.0,
                     ),
                   ),
                   child: CircleAvatar(
                     radius: 50,
-                    backgroundColor: Colors
-                        .white, // Set the avatar background color to white
+                    backgroundColor: Colors.white,
                     child: Stack(
                       children: <Widget>[
-                        // If you have an image to display, it should go here
                         Align(
                           alignment: Alignment.bottomRight,
                           child: Container(
                             decoration: BoxDecoration(
-                              color: Colors
-                                  .white, // Background color for the '+' icon
+                              color: Colors.white,
                               border: Border.all(
-                                color: Colors
-                                    .black, // Color of the border around the '+' icon
+                                color: Colors.black,
                               ),
-                              borderRadius: BorderRadius.circular(
-                                  12), // Circular border radius for the '+' icon background
+                              borderRadius: BorderRadius.circular(12),
                             ),
                             child: Icon(
                               Icons.add,
-                              color: Colors.black, // Icon color is black
-                              size: 24, // Icon size can be adjusted
+                              color: Colors.black,
+                              size: 24,
                             ),
                           ),
                         ),
@@ -79,7 +166,7 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 10), // Provide spacing between avatar and name
+            SizedBox(height: 10),
             Text(
               'Luciana González',
               style: TextStyle(
@@ -87,72 +174,55 @@ class ProfileScreen extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                   color: Colors.black),
             ),
-
             SizedBox(height: 30),
             Text('Ciudad de Río Cuarto, Córdoba.',
                 style: TextStyle(color: Colors.black, fontSize: 16.0)),
             Text('Miembro desde Diciembre 2023.',
                 style: TextStyle(color: Colors.black, fontSize: 16.0)),
-            SizedBox(height: 32.0), // Provide spacing before the button
+            SizedBox(height: 32.0),
             Container(
               height: 1.0,
-              color: Colors.grey, // Top border color grey
+              color: Colors.grey,
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
-                  vertical: 8.0,
-                  horizontal:
-                      16.0), // Adds margin vertically and horizontal padding around the button
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
               child: Center(
-                // Center the button within the Padding
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: AppColors
-                        .waterGreen400, // Set button background color to watergreen400
-                    onPrimary: Colors.white, // Set text color to white
-                    padding: EdgeInsets.symmetric(
-                        vertical: 8.0,
-                        horizontal:
-                            32.0), // Add padding inside the button for height
+                    primary: AppColors.waterGreen400,
+                    onPrimary: Colors.white,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
                   ),
-                  onPressed: () {
-                    // Handle reports
-                  },
+                  onPressed: () {},
                   child: Text(
                     'Reportes',
                     style: TextStyle(
-                      fontSize: 18.0, // Increase font size here
-                      // You can also adjust other text properties if needed
+                      fontSize: 18.0,
                     ),
                   ),
                 ),
               ),
             ),
-
             Container(
               height: 1.0,
-              color:
-                  AppColors.waterGreen400, // Bottom border color watergreen400
+              color: AppColors.waterGreen400,
             ),
-
             Expanded(
               child: Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                    vertical: 16.0), // Horizontal padding around all cards
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 16.0),
                 child: GridView.count(
                   crossAxisCount: 2,
-                  childAspectRatio:
-                      0.7, // Smaller value means wider cards, larger value means taller cards
-                  crossAxisSpacing: 16, // Horizontal space between the cards
-                  mainAxisSpacing: 16, // Vertical space between the cards
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
                   children: List.generate(2, (index) {
                     return Container(
                       decoration: BoxDecoration(
-                        color: Colors.white, // Card background color
-                        border: Border.all(color: Colors.black), // Black border
-                        borderRadius:
-                            BorderRadius.circular(32), // Border radius
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(32),
                       ),
                       child: Center(),
                     );
@@ -163,25 +233,33 @@ class ProfileScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: AppColors.waterGreen400),
-            label: '',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey, width: 1.0),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on, color: AppColors.waterGreen400),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.language, color: AppColors.waterGreen400),
-            label: '',
-          ),
-        ],
-        selectedItemColor: AppColors.waterGreen400,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: AppColors.waterGreen400),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_on, color: AppColors.waterGreen400),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.language, color: AppColors.waterGreen400),
+              label: '',
+            ),
+          ],
+          selectedItemColor: AppColors.waterGreen400,
+          unselectedItemColor: Colors.grey[100],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
       ),
     );
   }
